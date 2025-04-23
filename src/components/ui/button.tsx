8855bb8acfb0cb87404cc8baba138bde58ab7819
l0l0o <loccaille@gmail.com>
2025-04-23 17:16:03 +0200

    Ajout d'un nouveau composant Button avec des propriétés personnalisables pour le texte, les classes, les icônes et la taille du texte.

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,30 @@
+export default function Button({
+  text = "button",
+  className = "",
+  isIcon = false,
+  icon = "",
+  isSmallText = false,
+  rotateIcon = "rotate-180",
+}: {
+  text: string;
+  className?: string;
+  isIcon?: boolean;
+  icon?: string;
+  isSmallText?: boolean;
+  rotateIcon?: string;
+}) {
+  return (
+    <button
+      className={`h-8 bg-gray-100 rounded-lg px-4 py-2 flex flex-row items-center gap-0 ${className}`}
+    >
+      <span className={`font-semibold ${isSmallText ? "text-sm" : "text-md"}`}>
+        {text}
+      </span>
+      {isIcon && (
+        <picture className={`${rotateIcon} w-4 h-4`}>
+          <img src={icon} alt="logo" width={12} height={12} />
+        </picture>
+      )}
+    </button>
+  );
+}
