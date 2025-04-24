6efc578c48436de15b408e94d3467581c71b976c
l0l0o <loccaille@gmail.com>
2025-04-24 18:43:58 +0200

    Ajout du composant Story et mise à jour du composant StoryList pour afficher une liste d'histoires avec des images et des noms d'utilisateur.

--- src/components/sections/StoryList.tsx
+++ src/components/sections/StoryList.tsx
@@ -1,3 +1,11 @@
+import Story from "@/components/ui/Story";
+
 export default function StoryList() {
-  return <div>StoryList</div>;
+  return (
+    <div className="flex flex-row gap-8 px-6 mb-11">
+      <Story username="Compétences" isSvg={false} image="images/logo.svg" />
+      <Story username="Outils" isSvg={false} image="images/logo.svg" />
+      <Story username="Projets" isSvg={false} image="images/logo.svg" />
+    </div>
+  );
 }
---
+++ src/components/ui/Story.tsx
@@ -0,0 +1,27 @@
+import RoundedPicture from "./RoundedPicture";
+import Image from "next/image";
+export default function Story({
+  username,
+  isSvg = false,
+  image,
+}: {
+  username: string;
+  isSvg?: boolean;
+  image?: string;
+}) {
+  return (
+    <div className="flex flex-col gap-3 items-center px-3.5 py-2.5">
+      {image && (
+        <RoundedPicture hasBorder={true} size="md">
+          <Image
+            src={image}
+            alt={isSvg ? "logo" : username}
+            width={75}
+            height={75}
+          />
+        </RoundedPicture>
+      )}
+      <span className="text-xs font-semibold">{username}</span>
+    </div>
+  );
+}
