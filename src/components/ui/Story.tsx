e23ec73790a439ff8c1a2d84f3f2fddb5d416020
l0l0o <loccaille@gmail.com>
2025-04-25 12:18:41 +0200

    Refactorisation du composant Story : suppression des propriétés username et isSvg, ajout des propriétés alt et title pour une meilleure personnalisation des images et des titres.

--- src/components/ui/Story.tsx
+++ src/components/ui/Story.tsx
@@ -1,27 +1,22 @@
 import RoundedPicture from "./RoundedPicture";
 import Image from "next/image";
 export default function Story({
-  username,
-  isSvg = false,
   image,
+  alt = "",
+  title = "",
 }: {
-  username: string;
-  isSvg?: boolean;
   image?: string;
+  alt?: string;
+  title?: string;
 }) {
   return (
     <div className="flex flex-col gap-3 items-center px-3.5 py-2.5 cursor-pointer">
       {image && (
         <RoundedPicture hasBorder={true} size="md">
-          <Image
-            src={image}
-            alt={isSvg ? "logo" : username}
-            width={75}
-            height={75}
-          />
+          <Image src={image} alt={alt} width={75} height={75} />
         </RoundedPicture>
       )}
-      <span className="text-xs font-semibold">{username}</span>
+      <span className="text-xs font-semibold">{title}</span>
     </div>
   );
 }
