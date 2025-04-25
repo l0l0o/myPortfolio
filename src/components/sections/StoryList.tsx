dd463228f90422e04506705aa109ca62cab936f4
l0l0o <loccaille@gmail.com>
2025-04-25 12:18:50 +0200

    Modification du composant StoryList : remplacement de la propriété username par title pour une meilleure cohérence avec les changements précédents.

--- src/components/sections/StoryList.tsx
+++ src/components/sections/StoryList.tsx
@@ -3,9 +3,9 @@ import Story from "@/components/ui/Story";
 export default function StoryList() {
   return (
     <div className="flex flex-row gap-8 px-6 mb-11">
-      <Story username="Compétences" isSvg={false} image="images/logo.svg" />
-      <Story username="Outils" isSvg={false} image="images/logo.svg" />
-      <Story username="Projets" isSvg={false} image="images/logo.svg" />
+      <Story title="Compétences" image="images/logo.svg" />
+      <Story title="Outils" image="images/logo.svg" />
+      <Story title="Projets" image="images/logo.svg" />
     </div>
   );
 }
