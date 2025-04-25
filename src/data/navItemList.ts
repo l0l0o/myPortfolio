0e491b6d67f7ac54ce0a5887c620d2bf08695ad6
l0l0o <loccaille@gmail.com>
2025-04-25 15:30:42 +0200

    Ajout d'un nouveau fichier navItemList.ts contenant une liste d'éléments de navigation avec leurs titres et icônes respectives pour une meilleure structuration de la navigation.

---
+++ src/data/navItemList.ts
@@ -0,0 +1,44 @@
+import NavItem from "@/types/navItem.type";
+
+export const navItemList: NavItem[] = [
+  {
+    title: "Accueil",
+    icon: "icons/home.svg",
+    focusIcon: "icons/home_focus.svg",
+  },
+  {
+    title: "Recherche",
+    icon: "icons/search.svg",
+    focusIcon: "icons/search_focus.svg",
+  },
+  {
+    title: "Découvrir",
+    icon: "icons/discover.svg",
+    focusIcon: "icons/discover_focus.svg",
+  },
+  {
+    title: "Réels",
+    icon: "icons/reels.svg",
+    focusIcon: "icons/reels_focus.svg",
+  },
+  {
+    title: "Messages",
+    icon: "icons/send.svg",
+    focusIcon: "icons/send_focus.svg",
+  },
+  {
+    title: "Notifications",
+    icon: "icons/like.svg",
+    focusIcon: "icons/like_focus.svg",
+  },
+  {
+    title: "Créer",
+    icon: "icons/create.svg",
+    focusIcon: "icons/create_focus.svg",
+  },
+  {
+    title: "Profil",
+    icon: "icons/profile.svg",
+    focusIcon: "icons/profile_focus.svg",
+  },
+];
