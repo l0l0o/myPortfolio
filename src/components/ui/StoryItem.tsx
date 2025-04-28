1d0a071615ef8e4ee6a559a9e5fe8a6d0b580ae4
l0l0o <loccaille@gmail.com>
2025-04-28 15:58:46 +0200

    Refactor StoryItem component: streamline props by destructuring story object and improve image handling

--- src/components/ui/StoryItem.tsx
+++ src/components/ui/StoryItem.tsx
@@ -1,26 +1,24 @@
 import Link from "next/link";
 import RoundedPicture from "./RoundedPicture";
-import Image from "next/image";
+import Story from "@/types/story.type";
 
 export default function StoryItem({
-  image,
-  alt = "",
-  title = "",
-  id = "",
+  story: { id, image, title },
 }: {
-  image: string;
-  alt?: string;
-  title: string;
-  id: string;
+  story: Story;
 }) {
   return (
     <>
       <div className="flex flex-col gap-3 items-center px-3.5 py-2.5 cursor-pointer">
         {image && (
           <Link href={`/stories/highlights/${id}`}>
-            <RoundedPicture hasBorder={true} size="md">
-              <Image src={image} alt={alt} width={75} height={75} />
-            </RoundedPicture>
+            <RoundedPicture
+              hasBorder={true}
+              size="md"
+              src={image}
+              width={75}
+              height={75}
+            />
           </Link>
         )}
         <span className="text-xs font-semibold">{title}</span>
