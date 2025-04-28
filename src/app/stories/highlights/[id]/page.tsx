04035679b4aa89dc4991018a56a1ec2965421034
l0l0o <loccaille@gmail.com>
2025-04-28 18:22:41 +0200

    Add HighlightStory component: implement story display with media controls

---
+++ src/app/stories/highlights/[id]/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Logo from "@/components/ui/Logo";
+import Link from "next/link";
+import MediaDisplay from "@/components/ui/MediaDisplay";
+import { useStories } from "@/context/StoryProvider.context";
+import Story from "@/types/story.type";
+
+const HighlightStory = () => {
+  const params = useParams();
+  const id = params.id as string;
+
+  const [stories, setStories] = useState<Story[]>([]);
+  const [loading, setLoading] = useState(true);
+  const newStories = useStories();
+
+  useEffect(() => {
+    setStories(newStories);
+    setLoading(false);
+  }, [newStories]);
+
+  return (
+    <div className="absolute top-0 left-0 w-full h-full bg-black/90 flex items-center justify-center z-10">
+      <Logo className="absolute top-4 left-4" color="white" />
+      <Link
+        href={"/"}
+        className="absolute top-5 right-5 cursor-pointer p-2 size-9"
+      >
+        <Image src="/icons/close.svg" alt="logo" fill />
+      </Link>
+      <MediaDisplay story={stories[0]} isActive={true} />
+    </div>
+  );
+};
+
+export default HighlightStory;
