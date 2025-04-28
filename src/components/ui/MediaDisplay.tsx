f8570283120d1a4617bb99cd2431dcc292c6e933
l0l0o <loccaille@gmail.com>
2025-04-28 21:12:46 +0200

    Refactor MediaDisplay component: add storyIndex prop and implement hover effects with RoundedPicture

--- src/components/ui/MediaDisplay.tsx
+++ src/components/ui/MediaDisplay.tsx
@@ -7,12 +7,15 @@ import StoryTitle from "@/components/ui/StoryTitle";
 import Story from "@/types/story.type";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import RoundedPicture from "@/components/ui/RoundedPicture";
 
 const MediaDisplay = ({
   story,
+  storyIndex,
   isActive,
 }: {
   story: Story;
+  storyIndex: number;
   isActive: boolean;
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -64,7 +67,7 @@ const MediaDisplay = ({
               setCurrentIndex(currentIndex + 1);
             } else {
               setIsPlaying(false);
-              redirect("/");
+              redirect("/stories/highlights/" + (storyIndex + 1));
             }
           }
 
@@ -78,7 +81,7 @@ const MediaDisplay = ({
         clearInterval(timerRef.current);
       }
     };
-  }, [isActive, story, isPlaying, currentIndex, duration]);
+  }, [isActive, story, isPlaying, currentIndex, duration, storyIndex]);
 
   useEffect(() => {
     if (isActive && story) {
@@ -87,39 +90,88 @@ const MediaDisplay = ({
     }
   }, [isActive, story]);
 
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
       {story && isActive && (
-        <div className="h-screen max-w-[366px] w-full py-4">
-          <div className="w-full h-full bg-white relative rounded-md overflow-hidden">
-            <div className="absolute top-0 left-0 z-10 w-full flex flex-col gap-4 pt-5 px-4 pb-8 bg-gradient-to-b from-black to-transparent">
-              <div className="w-full flex flex-row gap-0.5">
-                {story.content.map((item, index) => (
-                  <ProgressBar
-                    key={index}
-                    progress={progressArray[index] || 0}
-                  />
-                ))}
-              </div>
-              <div className="w-full flex flex-row justify-between">
-                <StoryTitle
-                  src={story.image}
-                  title={story.title}
-                  width={10}
-                  height={10}
-                />
-                <VideoButton isPlaying={isPlaying} onClick={handlePlayPause} />
-              </div>
-            </div>
-            {currentIndex < story.content.length && (
-              <Image
-                src={story.content[currentIndex].image}
-                alt=""
-                fill
-                className="object-cover"
-              />
-            )}
+        <div className="flex flex-row items-center justify-center w-full gap-5">
+          <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={`h-screen flex items-center transition-all duration-300 ${
+              isHovered ? "" : "opacity-25"
+            }`}
+          >
+            <RoundedPicture
+              onClick={() => {}}
+              src={"/icons/chevron_navigation.svg"}
+              width={12}
+              height={12}
+              size="xs"
+              className={`rotate-180`}
+            />
           </div>
+          <div className="h-screen max-w-[366px] w-full py-4">
+            <div className="w-full h-full bg-white relative rounded-md overflow-hidden">
+              <div className="absolute top-0 left-0 z-10 w-full flex flex-col gap-4 pt-5 px-4 pb-8 bg-gradient-to-b from-black to-transparent">
+                <div className="w-full flex flex-row gap-0.5">
+                  {story.content.map((item, index) => (
+                    <ProgressBar
+                      key={index}
+                      progress={progressArray[index] || 0}
+                    />
+                  ))}
+                </div>
+                <div className="w-full flex flex-row justify-between">
+                  <StoryTitle
+                    src={story.image}
+                    title={story.title}
+                    width={10}
+                    height={10}
+                  />
+                  <VideoButton
+                    isPlaying={isPlaying}
+                    onClick={handlePlayPause}
+                  />
+                </div>
+              </div>
+              {currentIndex < story.content.length && (
+                <Image
+                  src={story.content[currentIndex].image}
+                  alt=""
+                  fill
+                  className="object-cover"
+                />
+              )}
+            </div>
+          </div>
+          <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={`h-screen flex items-center transition-all duration-300 ${
+              isHovered ? "" : "opacity-25"
+            }`}
+          >
+            <RoundedPicture
+              onClick={() => {}}
+              src={"/icons/chevron_navigation.svg"}
+              width={12}
+              height={12}
+              size="xs"
+            />
+          </div>
+        </div>
+      )}
+      {!isActive && (
+        <div className="relative w-[146px] h-[260px] bg-gray-500">
+          <div className=""></div>
         </div>
       )}
     </>
