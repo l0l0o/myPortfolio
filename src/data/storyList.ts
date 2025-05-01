fa1842544e59103b50b92a92c9c2ea94adc45f1c
l0l0o <loccaille@gmail.com>
2025-05-01 03:37:27 +0200

    Update storyList.ts: Replace image paths with correct assets for improved visual representation

--- src/data/storyList.ts
+++ src/data/storyList.ts
@@ -4,22 +4,62 @@ export const StoryList: Story[] = [
   {
     id: "0",
     title: "Projets",
-    image: "images/logo.svg",
+    image: "/stories/projet.webp",
     content: [
       {
-        image: "/images/test.webp",
-        time: 1,
+        image: "/stories/projets/pomo-pomo.webp",
+        time: 7,
       },
       {
-        image: "/images/next.svg",
-        time: 5,
+        image: "/stories/projets/pomo-pomo-1.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/pomo-pomo-2.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/pomo-pomo-3.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/mapbox-1.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/mapbox.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/mapbox-2.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/lol-stats.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/lol-stats-1.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/lol-stats-2.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/sicilien.webp",
+        time: 7,
+      },
+      {
+        image: "/stories/projets/sicilien-1.webp",
+        time: 7,
       },
     ],
   },
   {
     id: "1",
     title: "Stack",
-    image: "images/logo.svg",
+    image: "/stories/stack.webp",
     content: [
       {
         image: "/images/test.webp",
@@ -34,7 +74,7 @@ export const StoryList: Story[] = [
   {
     id: "2",
     title: "Outils",
-    image: "images/logo.svg",
+    image: "/stories/tools.webp",
     content: [
       {
         image: "/images/test.webp",
@@ -49,7 +89,7 @@ export const StoryList: Story[] = [
   {
     id: "3",
     title: "Mes à côtés",
-    image: "images/logo.svg",
+    image: "/stories/sides.webp",
     content: [
       {
         image: "/images/test.webp",
